e74f1a34a8c092909e5b7f6ab7d2524f74f4de76
shashank kadge <shashank_kadge@hotmail.com>
2023-12-03 00:14:50 -0500

    setTimeout added

--- script.js
+++ script.js
@@ -7,28 +7,33 @@ let selectArr = [['rock','fa-solid fa-hand-back-fist'],['paper','fa-solid fa-han
 for(let i = 0; i < icons.length; i++)
 {
     icons[i].addEventListener("click",function() {
-        let playerChoice = selectArr[i][0];
-        let ran = Math.floor(Math.random()*3);
-        let computerChoise = selectArr[ran][0];
-        
-        selectionContainer.innerHTML = `
-            <div class="you"><h2>You</h2><div class="icon"><i class="${selectArr[i][1]}"></i></div></div>
-            <div class="comp"><h2>COMPUTER</h2><div class="icon"><i class="${selectArr[ran][1]}"></i></div></div>          
-        `
-
-        if(playerChoice == computerChoise)
-        {
-            result.innerText = "DRAW";
-        } else if ((playerChoice == "rock" && computerChoise == "scissor") || 
-                    (playerChoice == "paper" && computerChoise == "rock") ||
-                    (playerChoice == "scissor" && computerChoise == "paper")) {
-                        result.innerText = "YOU WIN!";
-
-        } else {
-            result.innerText = "COMPUTER WINS!";
-        }
-         
+        result.innerText = "";
+        selectionContainer.innerHTML = "";
+        setTimeout(gameLogic,1000,i)
     })
+}
+
+function gameLogic(i){
+    let playerChoice = selectArr[i][0];
+    let ran = Math.floor(Math.random()*3);
+    let computerChoise = selectArr[ran][0];
+    
+    selectionContainer.innerHTML = `
+        <div class="you"><h2>You</h2><div class="icon"><i class="${selectArr[i][1]}"></i></div></div>
+        <div class="comp"><h2>COMPUTER</h2><div class="icon"><i class="${selectArr[ran][1]}"></i></div></div>          
+    `
+
+    if(playerChoice == computerChoise)
+    {
+        result.innerText = "DRAW";
+    } else if ((playerChoice == "rock" && computerChoise == "scissor") || 
+                (playerChoice == "paper" && computerChoise == "rock") ||
+                (playerChoice == "scissor" && computerChoise == "paper")) {
+                    result.innerText = "YOU WIN!";
+
+    } else {
+        result.innerText = "COMPUTER WINS!";
+    }
 }
 
 
